248d51e8101957f227708fa8314b429f34a0df99
gabe <gabrielmasampaio@gmail.com>
2025-05-04 20:59:49 -0400

    Refactor home page components, theme and tests. Add components to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import About from '@/components/home/About/About';
 import Hero from '@/components/home/Hero/Hero';
+import { Card, CardActions, CardContent, Typography } from '@mui/material';
+import Button from '@mui/material/Button';
 
 export default function Home() {
   return (
@@ -7,6 +9,56 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start max-w-4xl">
         <Hero />
         <About />
+        <div className="max-w-4xl flex gap-2">
+          <Card title="" className="min-w-full">
+            <CardContent>
+              <Typography
+                gutterBottom
+                sx={{ color: 'text.secondary', fontSize: 14 }}
+              >
+                Word of the Day
+              </Typography>
+              <Typography variant="h5" component="div">
+                be.nev.o.lent
+              </Typography>
+              <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>
+                adjective
+              </Typography>
+              <Typography variant="body2">
+                well meaning and kindly.
+                <br />
+                {'"a benevolent smile"'}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+          <Card title="Card 1" className="min-w-full">
+            <CardContent>
+              <Typography
+                gutterBottom
+                sx={{ color: 'text.secondary', fontSize: 14 }}
+              >
+                Word of the Day
+              </Typography>
+              <Typography variant="h5" component="div">
+                be.nev.o.lent
+              </Typography>
+              <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>
+                adjective
+              </Typography>
+              <Typography variant="body2">
+                well meaning and kindly.
+                <br />
+                {'"a benevolent smile"'}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+        </div>
       </main>
     </div>
   );
